1ce8d97ba261b23383b7dc08f219fb456b9308b5
Venkat S Rao <vrao423@gmail.com>
2016-10-14 23:03:54 -0400

    initial

--- ImageExportBug/ViewController.swift
+++ ImageExportBug/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var useNSData: UISwitch!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var labelCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var sharePressed: UIButton!
+    @IBAction func stepperChange(_ sender: UIStepper) {
+        labelCount.text = "\(sender.value)"
+    }
 
+    @IBAction func sharePressed(_ sender: UIButton) {
+        
+        let image = UIImage.init(named: "wide")
+        var items: [NSObject] = []
+        
+        let data = UIImageJPEGRepresentation(image!, 1.0)
+
+        
+        for _ in 1...Int(stepper.value) {
+            if useNSData.isOn {
+                items.append(data! as NSObject)
+            } else {
+                items.append(image!)
+            }
+        }
+        
+        let activityController = UIActivityViewController(activityItems: items, applicationActivities: nil);
+    
+        present(activityController, animated: true, completion: nil)
+    }
 }
 
